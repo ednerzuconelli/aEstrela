ee18fd9428d7229ae4f90216638af8b5b5355d34
ednerzuconelli <ednerzuconelli@gmail.com>
2016-07-11 17:31:20 -0300

    foi

--- src/aEstrela/AStar.java
+++ src/aEstrela/AStar.java
@@ -1,5 +1,5 @@
 package aEstrela;
-
+//http://computacao.gigamundo.com/exploracao-de-grafos-caixeiro-viajante-e-o-algoritmo-a-star-em-problemas-de-pathfinding/
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -18,19 +18,17 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
 
-//http://codereview.stackexchange.com/questions/38376/a-search-algorithm
-//http://www.codebytes.in/2015/02/a-shortest-path-finding-algorithm.html
-//http://www.codeproject.com/Articles/9880/Very-simple-A-algorithm-implementation
+
 final class NodeData<T> { 
 
     private final T nodeId;
-    private final Map<T, Double> heuristic;
+    private final Map<T, Integer> heuristic;
 
     private double g;  // g is distance from the source
     private double h;  // h is the heuristic of destination.
     private double f;  // f = g + h 
 
-    public NodeData (T nodeId, Map<T, Double> heuristic) {
+    public NodeData (T nodeId, Map<T, Integer> heuristic) {
         this.nodeId = nodeId;
         this.g = Double.MAX_VALUE; 
         this.heuristic = heuristic;
@@ -62,27 +60,20 @@ final class NodeData<T> {
     }
  }
 
-/**
- * The graph represents an undirected graph. 
- * 
- * @author SERVICE-NOW\ameya.patil
- *
- * @param <T>
- */
-final class GraphAStar<T> implements Iterable<T> {
+/*final class GraphAStar<T> implements Iterable<T> {
     /*
      * A map from the nodeId to outgoing edge.
      * An outgoing edge is represented as a tuple of NodeData and the edge length
      */
-    private final Map<T, Map<NodeData<T>, Double>> graph;
+   // private final Map<T, Map<NodeData<T>, Double>> graph;
     /*
      * A map of heuristic from a node to each other node in the graph.
      */
-    private final Map<T, Map<T, Double>> heuristicMap;
+   // private final Map<T, Map<T, Double>> heuristicMap;
     /*
      * A map between nodeId and nodedata.
      */
-    private final Map<T, NodeData<T>> nodeIdNodeData;
+  /*  private final Map<T, NodeData<T>> nodeIdNodeData;
 
     public GraphAStar(Map<T, Map<T, Double>> heuristicMap) {
         if (heuristicMap == null) throw new NullPointerException("The huerisic map should not be null");
@@ -90,22 +81,22 @@ final class GraphAStar<T> implements Iterable<T> {
         nodeIdNodeData = new HashMap<T, NodeData<T>>();
         this.heuristicMap = heuristicMap;
     } 
-
+*/
     /**
      * Adds a new node to the graph.
      * Internally it creates the nodeData and populates the heuristic map concerning input node into node data.
      * 
      * @param nodeId the node to be added
      */
-    public void addNode(T nodeId) {
+  /*  public void addNode(T nodeId) {
         if (nodeId == null) throw new NullPointerException("The node cannot be null");
         if (!heuristicMap.containsKey(nodeId)) throw new NoSuchElementException("This node is not a part of hueristic map");
 
         graph.put(nodeId, new HashMap<NodeData<T>, Double>());
         nodeIdNodeData.put(nodeId, new NodeData<T>(nodeId, heuristicMap.get(nodeId)));
     }
-
-    /**
+*/
+    /*
      * Adds an edge from source node to destination node.
      * There can only be a single edge from source to node.
      * Adding additional edge would overwrite the value
@@ -114,7 +105,7 @@ final class GraphAStar<T> implements Iterable<T> {
      * @param nodeIdSecond  the second node to be second node in the edge
      * @param length        the length of the edge.
      */
-    public void addEdge(T nodeIdFirst, T nodeIdSecond, double length) {
+  /*  public void addEdge(T nodeIdFirst, T nodeIdSecond, double length) {
         if (nodeIdFirst == null || nodeIdSecond == null) throw new NullPointerException("The first nor second node can be null.");
 
         if (!heuristicMap.containsKey(nodeIdFirst) || !heuristicMap.containsKey(nodeIdSecond)) {
@@ -127,42 +118,42 @@ final class GraphAStar<T> implements Iterable<T> {
         graph.get(nodeIdFirst).put(nodeIdNodeData.get(nodeIdSecond), length);
         graph.get(nodeIdSecond).put(nodeIdNodeData.get(nodeIdFirst), length);
     }
-
+*/
     /**
      * Returns immutable view of the edges
      * 
      * @param nodeId    the nodeId whose outgoing edge needs to be returned
      * @return          An immutable view of edges leaving that node
      */
-    public Map<NodeData<T>, Double> edgesFrom (T nodeId) {
+  /*  public Map<NodeData<T>, Double> edgesFrom (T nodeId) {
         if (nodeId == null) throw new NullPointerException("The input node should not be null.");
         if (!heuristicMap.containsKey(nodeId)) throw new NoSuchElementException("This node is not a part of hueristic map");
         if (!graph.containsKey(nodeId)) throw new NoSuchElementException("The node should not be null.");
 
         return Collections.unmodifiableMap(graph.get(nodeId));
     }
-
+*/
     /**
      * The nodedata corresponding to the current nodeId.
      * 
      * @param nodeId    the nodeId to be returned
      * @return          the nodeData from the 
      */ 
-    public NodeData<T> getNodeData (T nodeId) {
+  /*  public NodeData<T> getNodeData (T nodeId) {
         if (nodeId == null) { throw new NullPointerException("The nodeid should not be empty"); }
         if (!nodeIdNodeData.containsKey(nodeId))  { throw new NoSuchElementException("The nodeId does not exist"); }
         return nodeIdNodeData.get(nodeId);
     }
-
+*/
     /**
      * Returns an iterator that can traverse the nodes of the graph
      * 
      * @return an Iterator.
      */
-    @Override public Iterator<T> iterator() {
+  /*  @Override public Iterator<T> iterator() {
         return graph.keySet().iterator();
     }
-}
+}*/
 
 public class AStar<T> {
 
@@ -255,24 +246,24 @@ public class AStar<T> {
     	HashMap<Integer,Integer> resultado = new HashMap<Integer,Integer>();
     	HashMap<Integer,Integer> inicial = new HashMap<Integer,Integer>();
     	resultado.put(0, 1);
-    	resultado.put(1, 2);
-    	resultado.put(2, 3);
-    	resultado.put(3, 4);
-    	resultado.put(4, 12);
+    	resultado.put(1, 12);
+    	resultado.put(2, 11);
+    	resultado.put(3, 10);
+    	resultado.put(4, 2);
     	resultado.put(5, 13);
-    	resultado.put(6, 14);
-    	resultado.put(7, 5);
-    	resultado.put(8, 11);
-    	resultado.put(9, 0);
+    	resultado.put(6, 0);
+    	resultado.put(7, 9);
+    	resultado.put(8, 3);
+    	resultado.put(9, 14);
     	resultado.put(10, 15);
-    	resultado.put(11, 6);
-    	resultado.put(12, 10);
-    	resultado.put(13, 9);
-    	resultado.put(14, 8);
+    	resultado.put(11, 8);
+    	resultado.put(12, 4);
+    	resultado.put(13, 5);
+    	resultado.put(14, 6);
     	resultado.put(15, 7);
     	List<String> strings = new ArrayList<String>();
     	try {
-        	FileReader arq = new FileReader("E:/git/aEstrela/1.txt");
+        	FileReader arq = new FileReader("D:/Dowloads/caso/1.in");
             BufferedReader lerArq = new BufferedReader(arq);
             String linha = lerArq.readLine(); // lê a primeira linha
             strings.addAll(Arrays.asList(linha.split("(\\s{1})")));
@@ -287,11 +278,18 @@ public class AStar<T> {
     		inicial.put(i, Integer.parseInt(strings.get(i)));
     	}
     	
+    	
+    	
+		//if(abertos.isEmpty())
+		//	System.exit(1);
+		System.exit(0);
+
+    	
+    	
     	for (int i=0; i<inicial.size();i++){
     		if (!inicial.get(i).equals(resultado.get(i))){
     			cont1 +=1;
     		}	
-    		
     	}
     	System.out.println("heuristica 1 " + cont1);
     	System.out.println("heuristica 2 " + cont2);
